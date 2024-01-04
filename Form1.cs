6db24d7abae685ed0d9fc8b53416ae496cf0b108
shaheen memon <s.memon2294@student.leedsbeckett.ac.uk>
2024-01-04 19:37:01 +0000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using ProgrammingLibrary;
+using System;
+using System.Windows.Forms;
+
+namespace ASE_Programming_Language
+{
+    public partial class Form1 : Form
+    {
+        private Interpreter interpreter = new Interpreter();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string commandText = textBox1.Text;
+
+
+            ICommand command = ParseCommand(commandText);
+
+            if (command != null)
+            {
+                command.Execute(interpreter);
+                textBox2.Text = $"Value of {command.GetVariableName()}: {interpreter.GetVariableValue(command.GetVariableName())}";
+            }
+            else
+            {
+
+                textBox2.Text = "invalid command";
+            }
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private ICommand ParseCommand(string commandText)
+        {
+
+            string[] parts = commandText.Split(' ');
+
+            if (parts.Length == 4 && parts[0].ToLower() == "set" && parts[1].ToLower() == "valueof")
+            {
+                string variableName = parts[2];
+                int value;
+
+                if (int.TryParse(parts[3], out value))
+                {
+                    return new Commandinitialization(variableName, value);
+                }
+            }
+
+
+            return null;
+        }
+
+
+    }
+
+
+}
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ASE_Programming_Language
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
